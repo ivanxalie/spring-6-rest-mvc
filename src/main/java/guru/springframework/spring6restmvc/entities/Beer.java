071ff640d42dd59e924d8327bab67d696ae3e3a7
ivanxalie <ivanxalie@gmail.com>
2024-05-12 22:34:12 +0300

    Add mysql dependencies. Add pool configurations for additional performance.

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.math.BigDecimal;
@@ -22,7 +24,8 @@ public class Beer {
 
     @Id
     @GeneratedValue
-    @Column(nullable = false, updatable = false, length = 36, columnDefinition = "varchar")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(nullable = false, updatable = false, length = 36, columnDefinition = "varchar(36)")
     private UUID id;
 
     @Version
--- src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -2,6 +2,8 @@ package guru.springframework.spring6restmvc.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,7 +17,8 @@ import java.util.UUID;
 public class Customer {
     @GeneratedValue
     @Id
-    @Column(nullable = false, updatable = false, length = 36, columnDefinition = "varchar")
+    @Column(nullable = false, updatable = false, length = 36, columnDefinition = "varchar(36)")
+    @JdbcTypeCode(SqlTypes.CHAR)
     private UUID id;
     private String name;
     @Version
