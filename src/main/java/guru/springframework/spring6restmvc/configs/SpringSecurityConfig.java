73cb8aa01bb575c65e0591d0004ef35cd96f40ff
ivanxalie <ivanxalie@gmail.com>
2024-06-04 20:24:13 +0300

    Add actuator. Modify security to ignore actuator.

--- src/main/java/guru/springframework/spring6restmvc/configs/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/configs/SpringSecurityConfig.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6restmvc.configs;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,6 +15,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SpringSecurityConfig {
 
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
         return security
                 .oauth2ResourceServer(httpSecurity -> httpSecurity.jwt(withDefaults()))
@@ -20,6 +23,15 @@ public class SpringSecurityConfig {
                                 "/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html")
                         .permitAll()
                         .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity security) throws Exception {
+        return security
+                .authorizeHttpRequests(matcherRegistry -> matcherRegistry.requestMatchers(EndpointRequest.toAnyEndpoint())
+                        .permitAll())
                 .build();
     }
 }
