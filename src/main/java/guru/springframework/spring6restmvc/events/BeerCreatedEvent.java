70b35fb854dac6439786596ef1357af0546eeab3
ivanxalie <ivanxalie@gmail.com>
2024-06-10 22:01:50 +0300

    Fix constructor problems.

--- src/main/java/guru/springframework/spring6restmvc/events/BeerCreatedEvent.java
+++ src/main/java/guru/springframework/spring6restmvc/events/BeerCreatedEvent.java
@@ -1,12 +1,11 @@
 package guru.springframework.spring6restmvc.events;
 
 import guru.springframework.spring6restmvc.entities.Beer;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.Authentication;
 
-@Builder
+
 @Getter
 @Setter
 public class BeerCreatedEvent extends BeerEvent {
--- src/main/java/guru/springframework/spring6restmvc/events/BeerEvent.java
+++ src/main/java/guru/springframework/spring6restmvc/events/BeerEvent.java
@@ -1,17 +1,20 @@
 package guru.springframework.spring6restmvc.events;
 
 import guru.springframework.spring6restmvc.entities.Beer;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.Authentication;
 
 @Builder
-@AllArgsConstructor
 @Getter
 @Setter
 public class BeerEvent {
     private Beer beer;
     private Authentication authentication;
+
+    public BeerEvent(Beer beer, Authentication authentication) {
+        this.beer = beer;
+        this.authentication = authentication;
+    }
 }
